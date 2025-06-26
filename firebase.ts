24e6dd879daf42bc0b8e9172c65259d3e2445761
Jareem Hoff <jareem.e.hoff@gmail.com>
2025-06-25 22:54:26 -0400

    Revert "Merge branch 'main' of github.com:12jihan/jehoff-portfo-admin"
    This reverts commit 8425645aa4b6f42d8ba868af3582645fa1abfdb3, reversing
    changes made to c0d84a75ae658a48f14732c2faebd3e5f3ecb037.

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,7 @@
-import {
-  type FirebaseApp,
-  type FirebaseOptions,
-  initializeApp,
-} from "firebase/app";
+import { FirebaseOptions, initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
 // import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration
 const firebaseConfig: FirebaseOptions = {
@@ -21,12 +17,8 @@ const firebaseConfig: FirebaseOptions = {
 const app = initializeApp(firebaseConfig);
 
 // Init Firebase serivces
+// export const auth = getAuth(app);
 // export const storage = getStorage(app);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
 
 export default app;
-function getAuth(app: FirebaseApp) {
-  throw new Error("Function not implemented.");
-}
